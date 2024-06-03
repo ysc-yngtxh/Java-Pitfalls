3103cee13be1c590da4b6b9fdca61bd1545ae00d
游家纨绔 <youshicheng97@163.com>
2024-06-03 02:45:37 +0000

    update Demo2/src/main/java/com/example/Demo2Application.java. 上班时间，又偷摸使用手机提交
    Signed-off-by: 游家纨绔 <youshicheng97@163.com>

--- Demo2/src/main/java/com/example/Demo2Application.java
+++ Demo2/src/main/java/com/example/Demo2Application.java
@@ -9,7 +9,6 @@ public class Demo2Application {
     public static void main(String[] args) {
         SpringApplication.run(Demo2Application.class, args);
     }
-    // Demo2Application.class.getResourceAsStreaAsStream ()
-    // Demo2Application.class.getClassLoader().getResourceAsStreaAsStream(
-
+    // Demo2Application.class.getResourceAsStreaAsStream ()path 以“”/开头，因为默认的是此类所在包下取资源
+    // Demo2Application.class.getClassLoader().getResourceAsStreaAsStream(）不能以“/”开头，因为默认是从 classpath 根下获取，最终由 ClassLoader 获取资源
 }
