667f0964e952942d8a99f8788a6795f0d7733ae2
游家纨绔 <youshicheng97@163.com>
2024-06-12 04:02:16 +0000

    update Demo2/src/main/java/com/example/获取文件流/GetFileStream.java. 上班提交
    Signed-off-by: 游家纨绔 <youshicheng97@163.com>

--- Demo2/src/main/java/com/example/获取文件流/GetFileStream.java
+++ Demo2/src/main/java/com/example/获取文件流/GetFileStream.java
@@ -14,102 +14,30 @@ import java.net.URL;
  * @apiNote TODO
  */
 public class GetFileStream {
-
+    // 总结一下，就是你想获得文件，你得从最终生成的.class 文件为着手点，不要以.java 文件的路径为出发点，因为真正使用的就是.class，不会拿个.java 文件就使用，因为 Java 是编译型语言嘛
     @SneakyThrows
     public static void main(String[] args) {
 
-        // TODO 通过 Class类 获取资源
-        // 由于是通过 Class类 获取资源，默认的是从该类所在包下取资源。而info.txt文件编译后在当前类所在包下，因此无需以“/”开头
-        URL url1 = GetFileStream.class.getResource("info.txt");
-        System.out.println(url1.toURI().getPath());
-        // application.properties 文件在 resources 包中，编译后是直接在 target/classes 目录下的.
-        // 与当前类非同一层级包下，因此获取此类文件需要以“/”开头
-        URL url2 = GetFileStream.class.getResource("/application.properties");
-        System.out.println(url2.toURI().getPath());
+       // TODO 通过 Class类 获取资源
+       // 由于是通过 Class类 获取资源，默认的是从该类所在包下取资源。而info.txt文件编译后在当前类所在包下，因此无需以“/”开头
+       URL url1 = GetFileStream.class.getResource("info.txt");
+       System.out.println(url1.toURI().getPath());
+       // application.properties 文件在 resources 包中，编译后是直接在 target/classes 目录下的.
+       // 与当前类非同一层级包下，因此获取此类文件需要以“/”开头
+       URL url2 = GetFileStream.class.getResource("/application.properties");
+       System.out.println(url2.toURI().getPath());
 
-        InputStream inputStream = GetFileStream.class.getResourceAsStream("info.txt");
-        readerFileStream(inputStream);
-        // 使用 getResource 方法获取的路径，并非是文件在磁盘上的真实路径，而是虚拟路径。因此需要通过流的方式获取
-
-
-
-        // TODO 通过 ClassLoader类加载器 获取资源
-        // URL url3 = GetFileStream.class.getClassLoader().getResource("/info.txt");
-        // System.out.println(url3.toString());
-
-        // 由于是通过 ClassLoader类加载器 获取资源，默认是从 classpath 根下获取。
-        // 而 application.properties 文件编译后是在target/classes 目录下的，因此无需以“/”开头，
-        InputStream inputStream1 = GetFileStream.class.getClassLoader().getResourceAsStream("application.properties");
-        readerFileStream(inputStream1);
-    }
-    
-    public class GetFileStream {
-        // 总结一下，就是你想获得文件，你得从最终生成的.class 文件为着手点，不要以.java 文件的路径为出发点，因为真正使用的就是.class，不会拿个.java 文件就使用，因为 Java 是编译型语言嘛
-            @SneakyThrows
-                public static void main(String[] args) {
-
-                            // TODO 通过 Class类 获取资源
-                                    // 由于是通过 Class类 获取资源，默认的是从该类所在包下取资源。而info.txt文件编译后在当前类所在包下，因此无需以“/”开头
-                                            URL url1 = GetFileStream.class.getResource("info.txt");
-                                                    System.out.println(url1.toURI().getPath());
-                                                            // application.properties 文件在 resources 包中，编译后是直接在 target/classes 目录下的.
-                                                                    // 与当前类非同一层级包下，因此获取此类文件需要以“/”开头
-                                                                            URL url2 = GetFileStream.class.getResource("/application.properties");
-                                                                                    System.out.println(url2.toURI().getPath());
-
-                                                                                            InputStream inputStream = GetFileStream.class.getResourceAsStream("info.txt");
-                                                                                                    readerFileStream(inputStream);
-                                                                                                            // 使用 getResource 方法获取的路径，并非是文件在磁盘上的真实路径，而是虚拟路径。因此需要通过流的方式获取
+       InputStream inputStream = GetFileStream.class.getResourceAsStream("info.txt");
+       readerFileStream(inputStream)           
+       // 使用 getResource 方法获取的路径，并非是文件在磁盘上的真实路径，而是虚拟路径。因此需要通过流的方式获取
 
 
 
-                                                                                                                    // TODO 通过 ClassLoader类加载器 获取资源
-                                                                                                                            URL url3 = GetFileStream.class.getClassLoader().getResource("com/example/获取文件流/info.txt");
-                                                                                                                                    System.out.println(url3.toString());
+       // TODO 通过 ClassLoader类加载器 获取资源
+       URL url3 = GetFileStream.class.getClassLoader().getResource("com/example/获取文件流/info.txt");                                        System.out.println(url3.toString());
 
-                                                                                                                                            // 由于是通过 ClassLoader类加载器 获取资源，默认是从 classpath 根下获取。
-                                                                                                                                                    // 而 application.properties 文件编译后是在target/classes 目录下的，因此无需以“/”开头，
-                                                                                                                                                            InputStream inputStream1 = GetFileStream.class.getClassLoader().getResourceAsStream("application.properties");
-                                                                                                                                                                    readerFileStream(inputStream1);
-
-                                                                                                                                                                    public class GetFileStream {
-
-                                                                                                                                                                            @SneakyThrows
-                                                                                                                                                                                public static void main(String[] args) {
-
-                                                                                                                                                                                            // TODO 通过 Class类 获取资源
-                                                                                                                                                                                                    // 由于是通过 Class类 获取资源，默认的是从该类所在包下取资源。而info.txt文件编译后在当前类所在包下，因此无需以“/”开头
-                                                                                                                                                                                                            URL url1 = GetFileStream.class.getResource("info.txt");
-                                                                                                                                                                                                                    System.out.println(url1.toURI().getPath());
-                                                                                                                                                                                                                            // application.properties 文件在 resources 包中，编译后是直接在 target/classes 目录下的.
-                                                                                                                                                                                                                                    // 与当前类非同一层级包下，因此获取此类文件需要以“/”开头
-                                                                                                                                                                                                                                            URL url2 = GetFileStream.class.getResource("/application.properties");
-                                                                                                                                                                                                                                                    System.out.println(url2.toURI().getPath());
-
-                                                                                                                                                                                                                                                            InputStream inputStream = GetFileStream.class.getResourceAsStream("info.txt");
-                                                                                                                                                                                                                                                                    readerFileStream(inputStream);
-                                                                                                                                                                                                                                                                            // 使用 getResource 方法获取的路径，并非是文件在磁盘上的真实路径，而是虚拟路径。因此需要通过流的方式获取
-
-
-
-                                                                                                                                                                                                                                                                                    // TODO 通过 ClassLoader类加载器 获取资源
-                                                                                                                                                                                                                                                                                            // URL url3 = GetFileStream.class.getClassLoader().getResource("/info.txt");
-                                                                                                                                                                                                                                                                                                    // System.out.println(url3.toString());
-
-                                                                                                                                                                                                                                                                                                            // 由于是通过 ClassLoader类加载器 获取资源，默认是从 classpath 根下获取。
-                                                                                                                                                                                                                                                                                                                    // 而 application.properties 文件编译后是在target/classes 目录下的，因此无需以“/”开头，
-                                                                                                                                                                                                                                                                                                                            InputStream inputStream1 = GetFileStream.class.getClassLoader().getResourceAsStream("application.properties");
-                                                                                                                                                                                                                                                                                                                                    readerFileStream(inputStream1);
-                                                                                                                                                                                }
-
-                                                                                                                                                                                    private static void readerFileStream(InputStream inputStream) throws IOException {
-                                                                                                                                                                                                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                                                                                                                                                                                                        String readLine;
-                                                                                                                                                                                                                while ((readLine = bufferedReader.readLine()) != null) {
-                                                                                                                                                                                                                                System.out.println(readLine);
-                                                                                                                                                                                                                }
-                                                                                                                                                                                    }
-                                                                                                                                                                    }
+                                                                                                                                            // 由于是通过 ClassLoader类加载器 获取资源，默认是从 classpath 根下获取。                                                                    // 而 application.properties 文件编译后是在target/classes 目录下的，因此无需以“/"开头
+                                                                                                                                            InputStream inputStream1 = GetFileStream.class.getClassLoader().getResourceAsStream("application.properties");                        readerFileStream(inputStream1);
 
                                                                                                                                                                     // 使用系统的路径分隔符，进行组装路径
                                                                                                                                                                     String path = Thread.currentThread().getContextClassLoader().getResource().toURI().getPath()
